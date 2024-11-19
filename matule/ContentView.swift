68aceaf694d4caa6905d9366f6ae1395729cf9a2
evgeny <wxnnvdie@gmail.com>
2024-11-19 07:13:33 +0500

    Initial Commit

---
+++ matule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  matule
+//
+//  Created by Евгений Михайлов on 19.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ matule/matuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  matuleApp.swift
+//  matule
+//
+//  Created by Евгений Михайлов on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct matuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
